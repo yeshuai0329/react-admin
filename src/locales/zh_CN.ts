9c3944d3bc43ec767f122592199f5394b9665d94
yeshuai <yeshuai@divtoss.com>
2021-09-09 10:35:57 +0800

    refactor(国际化): 优化国际化方案

--- src/locales/zh_CN.ts
+++ src/locales/zh_CN.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-const ctx = require.context('./zh_CN', false, /\.ts/)
+const ctx = require.context('./zh_CN', false, /\.[tj]s/)
 
 export default ctx.keys().reduce((total: any, path: string) => {
   const requireContext = ctx(path).default
